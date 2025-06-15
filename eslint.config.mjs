63da833d59c74d2ec92753ff5a93664e1da7425c
Davide Bruno <davidebruno1989@gmail.com>
2025-06-15 05:20:41 +0000

    Add Prettier configuration and integrate ESLint with Prettier support. Update Typography component for improved readability and structure. Enhance package.json with Prettier and ESLint dependencies.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends('plugin:prettier/recommended'),
+  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends('next/typescript'),
+  {
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ];
-
-export default eslintConfig;
--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,30 +1,38 @@
 import { useMemo } from 'react';
-import { TailwindComponent, TailwindComponentProps, TailwindComponentTagName } from '../TailwindComponent';
+import {
+  TailwindComponent,
+  TailwindComponentProps,
+  TailwindComponentTagName,
+} from '../TailwindComponent';
 import { typographyStyles } from './styles';
 import { TypographyVariant } from './types';
 
-type TypographyProps<T extends TailwindComponentTagName = 'span'> = TailwindComponentProps<T> & {
+type TypographyProps<T extends TailwindComponentTagName = 'span'> =
+  TailwindComponentProps<T> & {
     variant?: TypographyVariant;
     secondary?: boolean;
-}
+  };
 
 export function Typography<T extends TailwindComponentTagName = 'span'>({
-    as = 'span' as T,
-    children,
-    variant = 'bodyMedium',
-    secondary = false,
-    ...props
+  as = 'span' as T,
+  children,
+  variant = 'bodyMedium',
+  secondary = false,
+  ...props
 }: TypographyProps<T>) {
-    const Component = as as React.ElementType;
-    const styles = useMemo(() =>
-        [typographyStyles[variant],
-        secondary && typographyStyles.secondary,
-        !secondary && typographyStyles.primary],
-        [variant, secondary]);
+  const Component = as as React.ElementType;
+  const styles = useMemo(
+    () => [
+      typographyStyles[variant],
+      secondary && typographyStyles.secondary,
+      !secondary && typographyStyles.primary,
+    ],
+    [variant, secondary]
+  );
 
-    return (
-        <TailwindComponent
-            as={Component}
-            styles={styles} {...props}>{children}</TailwindComponent>
-    );
-} 
+  return (
+    <TailwindComponent as={Component} styles={styles} {...props}>
+      {children}
+    </TailwindComponent>
+  );
+}
