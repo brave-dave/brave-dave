d7524cedd70ecafe26ed208cbdad5559d8b744f8
Davide Bruno <davidebruno1989@gmail.com>
2025-06-17 05:03:27 +0000

    Implement ContactCard component and update ContactSection to utilize it; remove ContactForm component for a simplified contact interface.

---
+++ src/sections/Contact/components/ContactCard/ContactCard.tsx
@@ -0,0 +1,13 @@
+import { Card } from '@/src/components/Card';
+import { Typography } from '@/src/components/Typography';
+
+export const ContactCard = () => {
+  return (
+    <Card>
+      <Typography as="p">
+        Whether it’s a quick hello or a longer conversation, feel free to drop
+        me a message. Always happy to chat
+      </Typography>
+    </Card>
+  );
+};
--- src/sections/Contact/sections/ContactSection/ContactSection.tsx
+++ src/sections/Contact/sections/ContactSection/ContactSection.tsx
@@ -1,16 +1,11 @@
-import ContactForm from '../../components/ContactForm';
+import { ContactCard } from '../../components/ContactCard';
 import { SectionStyles } from './styles';
 import { Section } from '@/src/components/Section';
 
 export function ContactSection() {
   return (
     <Section styles={SectionStyles}>
-      <p className="text-2xl font-bold">Let’s connect.</p>
-      <p className="text-lg">
-        Whether it’s a quick hello or a longer conversation, feel free to drop
-        me a message. Always happy to chat
-      </p>
-      <ContactForm />
+      <ContactCard />
     </Section>
   );
 }
